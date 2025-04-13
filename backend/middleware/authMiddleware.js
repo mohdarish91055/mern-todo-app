01363fc1515cb239b1cfe76e18ca8448dd08bd91
Arish <arish@gmail.com>
2025-04-13 23:50:33 +0530

    change cookie

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,27 +1,26 @@
-const dotenv = require('dotenv');
-const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 
 dotenv.config();
 
-const authMiddleware = async(req,res,next)=>{
-    const authHeader = req.cookies.token;
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.cookies.token;
+  console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).send({
+      message: "No token, authorization denied",
+    });
+  }
 
-    if(!authHeader){
-        return res.status(401).send({
-            message: 'No token, authorization denied',
-        })
-    }
-
-    const token = authHeader;
-    try{
-        const decoded = jwt.verify(token,process.env.SECRET_KEY);
-        req.user = decoded;
-        next();
-
-    }catch(error){
-        console.log('error in middleware',error);
-        res.status(401).json({ message: 'Token is not valid' });
-    }
-}
+  const token = authHeader;
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log("error in middleware", error);
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
 
 module.exports = authMiddleware;
